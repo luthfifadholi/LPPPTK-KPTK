2f0cecd81ee934d9422e8f570e53a993f8ee6452
luthfifadholi <32456523+luthfifadholi@users.noreply.github.com>
2018-04-26 11:13:12 +0700

    First commit

---
+++ index.php
@@ -0,0 +1,224 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>KPTK - Home</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="assets/plugins/bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="assets/plugins/bootstrap/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script> -->
+
+    <script type="text/javascript" src="assets/plugins/bootstrap/js/bootstrap.js"></script>
+    <script type="text/javascript" src="assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+  <!-- <link rel="stylesheet" type="text/css" href="assets/plugins/creative/css/creative.css"> -->
+  <!-- <link rel="stylesheet" type="text/css" href="assets/plugins/creative/css/creative.min.css"> -->
+    <style>
+        /* Make the image fully responsive */
+        .carousel-inner img {
+            width: 100%;
+            height: 50vh;
+        }
+    </style>
+	<link rel="stylesheet" type="text/css" href="assets/css/index.css">
+</head>
+<body>
+	<!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+        <div class="container">
+            <a class="navbar-brand js-scroll-trigger" href="#page-top">KPTK</a>
+            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="">Beranda</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="">Profil Lembaga</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="">Struktur Organisasi</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="">Artikel</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <div id="demo" class="carousel slide" data-ride="carousel">
+
+        <!-- Indicators -->
+        <ul class="carousel-indicators">
+            <li data-target="#demo" data-slide-to="0" class="active"></li>
+            <li data-target="#demo" data-slide-to="1"></li>
+            <li data-target="#demo" data-slide-to="2"></li>
+        </ul>
+
+        <!-- The slideshow -->
+        <div class="carousel-inner">
+            <div class="carousel-item active first-image">
+                <div class="layer"></div>
+                <div class="carousel-caption">
+                    <h3>Ini Judul Slide Pertama</h3>
+                    <p>Ini Deskripsinya</p>
+                </div>
+            </div>
+            <div class="carousel-item second-image">
+                <div class="layer"></div>
+                <div class="carousel-caption">
+                    <h3>Ini Judul Slide Kedua</h3>
+                    <p>Ini Deskripsinya</p>
+                </div>
+            </div>
+            <div class="carousel-item third-image">
+                <div class="layer"></div>
+                <div class="carousel-caption">
+                    <h3>Ini Judul Slide Ketiga</h3>
+                    <p>Ini Deskripsinya</p>
+                </div>
+            </div>
+        </div>
+
+        <!-- Left and right controls -->
+        <a class="carousel-control-prev" href="#demo" data-slide="prev">
+            <!-- <span class="carousel-control-prev-icon"></span> -->
+        </a>
+        <a class="carousel-control-next" href="#demo" data-slide="next">
+            <!-- <span class="carousel-control-next-icon"></span> -->
+        </a>
+    </div>
+
+    <section>
+        <div class="about-section">
+            <div class="section-wrapper">
+                <div class="row">
+                    <div class="description col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                        <h3>Tentang Instansi</h3>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </p>
+                        <button>Selengkapnya</button>
+                    </div>
+                    <div class="photo col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                        <div class="trapezium"></div>
+                        <img src="assets/images/1.png">
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="latest-article">
+            <h2>Artikel Terbaru</h2>
+            <div class="section-wrapper">
+                <div class="row">
+                    <div class="col-lg-4 col-md-6 col-sm-12 col-xs-12">
+                        <div class="card">
+                            <img class="card-img-top" src="assets/images/1.png" alt="Card image" style="width:100%">
+                            <div class="card-body">
+                                <strong>Ini Judul Berita Pada Artikel Ini</strong>
+                                <span>Teknologi Informasi dan Komunikasi</span>
+                                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo</p>
+                                <a href="#" class="read-more-btn">Selengkapnya</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 col-sm-12 col-xs-12">
+                        <div class="card">
+                            <img class="card-img-top" src="assets/images/1.png" alt="Card image" style="width:100%">
+                            <div class="card-body">
+                                <strong>Ini Judul Berita Pada Artikel Ini</strong>
+                                <span>Teknologi Informasi dan Komunikasi</span>
+                                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo</p>
+                                <a href="#" class="read-more-btn">Selengkapnya</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 col-sm-12 col-xs-12">
+                        <div class="card">
+                            <img class="card-img-top" src="assets/images/1.png" alt="Card image" style="width:100%">
+                            <div class="card-body">
+                                <strong>Ini Judul Berita Pada Artikel Ini</strong>
+                                <span>Teknologi Informasi dan Komunikasi</span>
+                                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo</p>
+                                <a href="#" class="read-more-btn">Selengkapnya</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 col-sm-12 col-xs-12">
+                        <div class="card">
+                            <img class="card-img-top" src="assets/images/1.png" alt="Card image" style="width:100%">
+                            <div class="card-body">
+                                <strong>Ini Judul Berita Pada Artikel Ini</strong>
+                                <span>Teknologi Informasi dan Komunikasi</span>
+                                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo</p>
+                                <a href="#" class="read-more-btn">Selengkapnya</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 col-sm-12 col-xs-12">
+                        <div class="card">
+                            <img class="card-img-top" src="assets/images/1.png" alt="Card image" style="width:100%">
+                            <div class="card-body">
+                                <strong>Ini Judul Berita Pada Artikel Ini</strong>
+                                <span>Teknologi Informasi dan Komunikasi</span>
+                                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo</p>
+                                <a href="#" class="read-more-btn">Selengkapnya</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 col-sm-12 col-xs-12">
+                        <div class="card">
+                            <img class="card-img-top" src="assets/images/1.png" alt="Card image" style="width:100%">
+                            <div class="card-body">
+                                <strong>Ini Judul Berita Pada Artikel Ini</strong>
+                                <span>Teknologi Informasi dan Komunikasi</span>
+                                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo</p>
+                                <a href="#" class="read-more-btn">Selengkapnya</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="read-all-section">
+                <a href="" class="read-all-btn">Lihat Semua</a>
+                </div>
+            </div>
+        </div>
+        <div class="bottom-logo">
+            <div class="section-wrapper">
+                <div class="row">
+                    <div class="col-lg-2 col-md- ">
+                        jjhjhjhj
+                    </div>
+                    <div class="col-lg-2 col-md- ">
+                        jjhjhjhj
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- <script type="text/javascript" src="assets/plugins/creative/js/creative.js"></script> -->
+    <!-- <script type="text/javascript" src="assets/plugins/creative/js/creative.min.js"></script> -->
+</body>
+</html>
